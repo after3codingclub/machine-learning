bf384c13f5f7af11a3885f3c0c96cb439986d77a
MDN <mark.denardo@gmail.com>
2023-10-24 14:16:35 -0400

    Create week-06.md

---
+++ fall-2023/week-06.py
@@ -0,0 +1,2 @@
+<h3>using python turtle</h3>
+<p>using python turtle we're going to explore making some shapes as creative code and also explore the differences btw imperative and declarative programming</p>
