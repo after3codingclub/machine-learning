a872d6974da0c9f3a076a857f77379773107bda5
MDN <mark.denardo@gmail.com>
2023-10-24 14:12:50 -0400

    Update rps-00.py

--- fall-2023/week-05/rps-00.py
+++ fall-2023/week-05/rps-00.py
@@ -1,1 +1,24 @@
+p1 = input("enter rock, paper, or scissors")
+p2 = input("enter rock, paper, or scissors")
 
+if p1 == "rock" and p2 == "scissors":
+  print("p1 wins")
+elif p1 == "paper" and p2 == "scissors":
+  print("p2 wins")
+elif p1 == "scissors" and p2 == "scissors":
+  print("tie")
+elif p1 == "rock" and p2 == "paper":
+  print("p2 wins")
+elif p1 == "paper" and p2 == "paper":
+  print("tie")
+elif p1 == "scissors" and p2 == "paper":
+  print("p1 wins")
+elif p1 == "rock" and p2 == "rock":
+  print("tie")
+elif p1 == "paper" and p2 == "rock":
+  print("p1 wins")
+elif p1 == "scissors" and p2 == "rock":
+  print("p2 wins")
+else:
+print("invalid input")
+  
