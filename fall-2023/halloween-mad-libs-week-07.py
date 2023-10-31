c85b06224c80543f858b1d8ddeb8e5b1589b001e
MDN <mark.denardo@gmail.com>
2023-10-31 12:25:52 -0400

    Create halloween-mad-libs-week-07.py

---
+++ fall-2023/halloween-mad-libs-week-07.py
@@ -0,0 +1,17 @@
+def halloween_madlibs():
+    print("Welcome to the Halloween Madlibs Game!")
+    
+    # Get user inputs
+    adjective = input("Give me an adjective: ")
+    plural_noun = input("Enter a plural noun: ")
+    verb = input("Give me a verb: ")
+    place = input("Name a spooky place: ")
+    creature = input("Name a Halloween creature: ")
+
+    # Display the story
+    print("\nHere's your spooky Halloween story:")
+    print(f"On a {adjective} Halloween night, a group of {plural_noun} decided to {verb}.")
+    print(f"They ventured into the haunted {place} where they encountered a terrifying {creature}!")
+
+# Run the madlibs game
+halloween_madlibs()
